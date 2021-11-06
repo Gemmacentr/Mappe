534cf32b48b0e386be07d6d1c2f04d6b12e43fb7
Gemma Centrone <56439561+Gemmacentr@users.noreply.github.com>
2021-11-06 16:37:19 +0100

    Add files via upload

---
+++ Mappe/OpenStreet7/js.js
@@ -0,0 +1,10 @@
+const mymap = L.map('mapid').setView([44.502379, 11.338829], 13);
+
+mapLink =
+    '<a href="https://openstreetmap.org" target="_blank">OpenStreetMap</a>';
+L.tileLayer(
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 39,
+    clickable: true
+}).addTo(mymap);
